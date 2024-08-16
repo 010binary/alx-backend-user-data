7e3dcaec9f8820bc339798c434604e25fb2f27b2
010binary <augustine32fg@gmail.com>
2024-08-16 00:01:53 +0000

    db

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
+"""DB module
 """
-you will complete the DB class provided below to implement the add_user method
-"""
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
-from user import Base
+
+from user import Base, User
 
 
 class DB:
@@ -33,37 +34,31 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Adds a new user to the database and returns the User object
-        """
+        """Add a new user to the database"""
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
         return new_user
 
     def find_user_by(self, **kwargs) -> User:
-        """Finds a user by arbitrary keyword arguments
-        Raises:
-            NoResultFound: If no user is found
-            InvalidRequestError: If invalid query arguments are passed
-        """
+        """Find a user by arbitrary attributes"""
         try:
-            query = self._session.query(User).filter_by(**kwargs)
-            user = query.one()
+            user = self._session.query(User).filter_by(**kwargs).one()
             return user
         except NoResultFound:
-            raise NoResultFound("No user found with the provided filters.")
+            raise NoResultFound()
         except InvalidRequestError:
-            raise InvalidRequestError("Invalid query arguments provided.")
+            raise InvalidRequestError()
 
     def update_user(self, user_id: int, **kwargs) -> None:
-        """Updates a user's attributes and commits the changes to the database
-        Raises:
-            ValueError: If an argument does not correspond to a valid user attribute
-        """
-        user = self.find_user_by(id=user_id)
-        for key, value in kwargs.items():
-            if hasattr(user, key):
-                setattr(user, key, value)
-            else:
-                raise ValueError(f"Invalid attribute: {key}")
-        self._session.commit()
+        """Update user attributes"""
+        try:
+            user = self.find_user_by(id=user_id)
+            for key, value in kwargs.items():
+                if hasattr(user, key):
+                    setattr(user, key, value)
+                else:
+                    raise ValueError(f"Invalid attribute: {key}")
+            self._session.commit()
+        except NoResultFound:
+            raise ValueError("User not found")
